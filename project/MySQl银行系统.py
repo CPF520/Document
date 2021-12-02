5b931a3cadccfe6fc3d3687e60497acbca4dd1e3
hoodliganâ <1839157125@qq.com>
2021-12-02 19:30:03 +0800

    demo

--- project/MySQl银行系统.py
+++ project/MySQl银行系统.py
@@ -7,194 +7,209 @@ bank = {}
 # 银行名称
 bank_name = "中国工商银行昌平支行"
 
-
-def welcome():
-    print("---------------------------------------")
-    print("-      中国工商银行账户管理系统V1.0       -")
-    print("--------------------------------------")
-    print("-  1.开户                             -")
-    print("-  2.存钱                             -")
-    print("-  3.取钱                             -")
-    print("-  4.转账                             -")
-    print("-  5.查询                             -")
-    print("-  6.Bye!                            -")
-    print("--------------------------------------")
+class Cwelcome:
+    def welcome(self):
+        print("---------------------------------------")
+        print("-      中国工商银行账户管理系统V1.0       -")
+        print("--------------------------------------")
+        print("-  1.开户                             -")
+        print("-  2.存钱                             -")
+        print("-  3.取钱                             -")
+        print("-  4.转账                             -")
+        print("-  5.查询                             -")
+        print("-  6.Bye!                            -")
+        print("--------------------------------------")
 
 
 # 银行的开户逻辑
-def bank_addUser(account, username, password, country, province, street, door, money):
-    # 判断是否已满
-    sql = "select count(*) from system"
-    param = []
-    data = select(sql, param, mode="one")  # (4,)
-    if data[0][0] > 100:
-        return 3
-    # 查询是否存在
-    sql1 = "select * from system where username  = %s"
-    param1 = [username]
-    data1 = select(sql1, param1, mode="all")  # ()
+class Cbank_addUser:
+    def bank_addUser(self,account, username, password, country, province, street, door, money):
+        # 判断是否已满
+        sql = "select count(*) from system"
+        param = []
+        data = select(sql, param, mode="one")  # (4,)
+        if data[0][0] > 100:
+            return 3
+        # 查询是否存在
+        sql1 = "select * from system where username  = %s"
+        param1 = [username]
+        data1 = select(sql1, param1, mode="all")  # ()
 
-    # 判断是否开过户
-    if len(data1) > 0:
-        return 2
+        # 判断是否开过户
+        if len(data1) > 0:
+            return 2
 
-    # 插入数据
-    sql2 = "insert into system values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    param2 = [account, username, password, country, province, street, door, money, "2021-11-22", bank_name]
-    update(sql2, param2)
-    return 1
+        # 插入数据
+        sql2 = "insert into system values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        param2 = [account, username, password, country, province, street, door, money, "2021-11-22", bank_name]
+        update(sql2, param2)
+        return 1
 
 
 # 开户的输入数据
-def addUser():
-    username = input("请输入姓名：")
+class CaddUser(Cbank_addUser):
+    def addUser(self):
+        username = input("请输入姓名：")
 
-    password = input("请输入密码：")
+        password = input("请输入密码：")
 
-    country = input("请输入国籍：")
+        country = input("请输入国籍：")
 
-    province = input("请输入省份：")
+        province = input("请输入省份：")
 
-    street = input("请输入街道：")
-    door = input("请输入您家门牌号：")
+        street = input("请输入街道：")
+        door = input("请输入您家门牌号：")
 
-    money = int(input("请输入初始化您的银行卡余额："))
+        money = int(input("请输入初始化您的银行卡余额："))
 
-    account = random.randint(10000000, 99999999)
+        account = random.randint(10000000, 99999999)
 
-    status = bank_addUser(account, username, password, country, province, street, door, money)
+        status = super().bank_addUser(account, username, password, country, province, street, door, money)
 
-    if status == 3:
-        print("对不起，该银行用户已满，请携带证件到其他银行办理！")
-    elif status == 2:
-        print("您之前已经开过户！禁止重复开户！")
-    elif status == 1:
-        print("嘻嘻，开户成功！以下卡户的个人信息：")
-        info = '''
-            ------------个人信息查询结果-------------
-            用户名：%s
-            账号：%s
-            密码：%s
-            地址：
-                国籍：%s
-                省份：%s
-                街道：%s
-                门牌号：%s
-            余额：%s
-            开户行名称：%s
-            ---------------------------------------
-        '''
-        print(info % (username, account, password, country, province, street, door, money, bank_name))
-        print(bank)
+        if status == 3:
+            print("对不起，该银行用户已满，请携带证件到其他银行办理！")
+        elif status == 2:
+            print("您之前已经开过户！禁止重复开户！")
+        elif status == 1:
+            print("嘻嘻，开户成功！以下卡户的个人信息：")
+            info = '''
+                ------------个人信息查询结果-------------
+                用户名：%s
+                账号：%s
+                密码：%s
+                地址：
+                    国籍：%s
+                    省份：%s
+                    街道：%s
+                    门牌号：%s
+                余额：%s
+                开户行名称：%s
+                ---------------------------------------
+            '''
+            print(info % (username, account, password, country, province, street, door, money, bank_name))
+
 
 
 # 银行存钱逻辑：
-def bank_saveMone(ID, sum):
-    sql = "select * from system where account=%s"
-    param = [ID]
-    data = select(sql, param, mode="all")
-    # 判断 data是不是一个空元组（）   是空元组返回False  不是执行update_addmoney函数进行存钱操作
-    if not data:
-        print("该账号不存在！")
-        return False
-    else:
-        sql1 = "update system set money = money+%s where account =%s"
-        param1 = [sum,ID]
-        update(sql1, param1)
-        print("存钱成功！")
+class Cbank_saveMone:
+    def bank_saveMone(self,ID, sum):
+        sql = "select * from system where account=%s"
+        param = [ID]
+        data = select(sql, param, mode="all")
+        # 判断 data是不是一个空元组（）   是空元组返回False  不是执行update_addmoney函数进行存钱操作
+        if not data:
+            print("该账号不存在！")
+            return False
+        else:
+            sql1 = "update system set money = money+%s where account =%s"
+            param1 = [sum,ID]
+            update(sql1, param1)
+            print("存钱成功！")
 
 
 # 存钱输入数据：
-def saveMoney():
-    ID = int(input('请输入账号'))
-    sum = int(input('请输入存钱的金额'))
-    find = bank_saveMone(ID, sum)
+class CssaveMoney(Cbank_saveMone):
+    def saveMoney(self):
+        ID = int(input('请输入账号'))
+        sum = int(input('请输入存钱的金额'))
+        find = bank_saveMone(ID, sum)
 
-    find == False
+        find == False
 
 
 # 取钱逻辑
-def bank_draw(number, coded, amt):
-    sql = "select * from system where account=%s"
-    param = [number]
-    data = select(sql, param, mode="all")
-    # 判断 data是不是一个空元组（）   是空元组返回1
-    if not data:
-        return 1
-    if coded != data[0][2]:
-        return 2
-    if amt > data[0][7]:
-        return 3
-    else:
-        sql1 = "update system set money=money-%s where account=%s and password=%s"
-        param1 = [amt,number, coded]
-        update(sql1, param1)
-        print("取钱成功")
+class Cbank_draw:
+    def bank_draw(self,number, coded, amt):
+        sql = "select * from system where account=%s"
+        param = [number]
+        data = select(sql, param, mode="all")
+        # 判断 data是不是一个空元组（）   是空元组返回1
+        if not data:
+            return 1
+        if coded != data[0][2]:
+            return 2
+        if amt > data[0][7]:
+            return 3
+        else:
+            sql1 = "update system set money=money-%s where account=%s and password=%s"
+            param1 = [amt,number, coded]
+            update(sql1, param1)
+            print("取钱成功")
 
 
 # 取钱输入数据
-def draw():
-    number = int(input('请输入账号'))
-    coded = input("请输入密码")
-    amt = int(input("请输入取钱的金额"))
-    find = bank_draw(number, coded, amt)
+class Cdraw(Cbank_draw):
+    def draw(self):
+        number = int(input('请输入账号'))
+        coded = input("请输入密码")
+        amt = int(input("请输入取钱的金额"))
+        find = bank_draw(number, coded, amt)
 
-    if find == 1:
-        print("账号不存在")
-    if find == 2:
-        print("密码不对")
-    if find == 3:
-        print("钱不够")
+        if find == 1:
+            print("账号不存在")
+        if find == 2:
+            print("密码不对")
+        if find == 3:
+            print("钱不够")
 
 
 # 查询逻辑
-def bank_query(af, bf):
-    sql = "select * from system where account  = %s"
-    param = [af]
-    data = select(sql, param, mode="all")
-    if not data:
-        print("该用户不存在")
-        return 1
-    if bf != data[0][2]:
-        return 2
-    else:
-            print("-------------查询的账号信息------------")
-            print("用户名：", data[0][1])
-            print("账号：", data[0][0])
-            print("密码：", data[0][2])
-            print("国籍：", data[0][3])
-            print("省份：", data[0][4])
-            print("街道：", data[0][5])
-            print("门牌号：", data[0][6])
-            print("余额：", data[0][7])
-            print("开户日期：", data[0][8])
-            print("开户银行：", data[0][9])
+class Cbankquery:
+    def bank_query(self,af, bf):
+        sql = "select * from system where account  = %s"
+        param = [af]
+        data = select(sql, param, mode="all")
+        if not data:
+            print("该用户不存在")
+            return 1
+        if bf != data[0][2]:
+            return 2
+        else:
+                print("-------------查询的账号信息------------")
+                print("用户名：", data[0][1])
+                print("账号：", data[0][0])
+                print("密码：", data[0][2])
+                print("国籍：", data[0][3])
+                print("省份：", data[0][4])
+                print("街道：", data[0][5])
+                print("门牌号：", data[0][6])
+                print("余额：", data[0][7])
+                print("开户日期：", data[0][8])
+                print("开户银行：", data[0][9])
 
 
 # 查询输入数据
-def query():
-    af = int(input("请输入用户账号"))
-    bf = input("请输入密码")
-    find = bank_query(af, bf)
-    if find == 1:
-        print("账号不存在")
-    if find == 2:
-        print("密码不对")
+class Cquery(Cbankquery):
+    def query(self):
+        af = int(input("请输入用户账号"))
+        bf = input("请输入密码")
+        find = bank_query(af, bf)
+        if find == 1:
+            print("账号不存在")
+        if find == 2:
+            print("密码不对")
 
 
+class Cwhile(Cwelcome):
+    def wwhile(self):
+        while True:
+            super().welcome()
+            chose = input("请输入业务编号：")
+            if chose == "1":
+                a = CaddUser()
+                a.addUser()
+            elif chose == "2":
+                a = CsaveMoney()
+                a.saveMoney()
+            elif chose == "3":
+                a = Cdraw()
+                a.draw()
+            elif chose == "4":
+                pass
+            elif chose == "5":
+                a = Cquery()
+                a.query()
+            elif chose == "6":
+                pass
 
-while True:
-    welcome()
-    chose = input("请输入业务编号：")
-    if chose == "1":
-        addUser()
-    elif chose == "2":
-        saveMoney()
-    elif chose == "3":
-        draw()
-    elif chose == "4":
-        pass
-    elif chose == "5":
-        query()
-    elif chose == "6":
-        pass
+Cwhile().wwhile()
--- project/猜数字.py
+++ project/猜数字.py
@@ -1,36 +1,88 @@
 '''
-1、猜的数字是系统产生，不是自定义
-        使用random随机数技术来获取随机数
-范围：0~100
-    猜10次！
-    如果输入大了：温馨提示：大了
-    如果输入小了：温馨提示：小了
-    正好猜中，恭喜您，猜中，本次猜的数字为XXX
-操作完成之后才增加：
-    起始：5000金币，每猜错一次，减去500金币，一直扣完为止。10次没猜中，系统锁定。猜中加3000。
-'''
+需求：
+1、猜的数字是系统产生的，不是自己定义的
+2、键盘输入的   操作完填入：input（“提示”）
+3、判断			操作完填入：if判断条件 elif 判断条件。。。。。。Else
+4、循环			操作完填入：while 条件循环
+任务：如果键盘输入大于随机数弹出友好提示信息“猜大了”，猜小了
+起始金额  5000 才对一次给300 猜错扣除100 猜错15次结束
 
 import random
-
-C = random.randint(0, 100)
-print(C)
-gold = 5000
-print("您的初始金币为：5000" ,end="")
-print("猜对加3000，猜错减500，您共有10次机会")
-i = 1
-while i <= 10:
-    num = input("请输入你猜的数字")
-    num = int(num)
-    if num == C:
-        gold += 3000
-        print("恭喜您猜中，本次的数字为 %d" % C)
-        print("ok")
-    elif C < num:
-        gold += 500
-        print("您输入的数字太大了")
+#   随机生成数字  （开始int，结束int）  []
+while True:
+    Ran = random.randint(1, 20)
+    num=input("请输入一个数字")
+    num=int(num)
+    #  键盘输入  随机数
+    if num == Ran:
+        print("猜对了")
+        break
     else:
-        gold -= 500
-        print("您输入的数字太小了")
+        print("猜错了")
+
+i =input("请输入数字")
+i=int(i)#等于号后面的是之前的变量，等于号前面的是新变量
+if i == 1:
+    print("ok")
+elif i >4:
+    print("ok2")
+elif i<2:
+    print("ok5")
 else:
     print("no")
-    i = i + 1
+
+for
+while
+
+
+
+i=0
+while i<12: # 条件为T F
+    i=i+1#
+    if i == 3:
+        continue
+    print(i)#打印i
+
+    while循环  if如果  continue跳出此循环  break跳出整个循环  int取整  print打印  input默认为字符串类型 else否则 True对
+    random开始.randint结束 input（“提示”）
+'''
+import random #导入模块
+money=5000
+I=15
+B=0
+print("持有现金:",money)
+T = random.randint(1, 20)
+while True:#这是一个循环
+    print("正确答案:",T)
+    E=input("请输入一个1-20的数字")
+    E=int(E)
+    if T == E:
+        print("BING")
+        money=money+3000
+        print("持有现金:",money)
+        T = random.randint(1, 20)
+        continue
+    else:
+        print("Try Again")
+        money=money-100
+        I=I-1
+        print("持有现金:",money)
+        print("剩余次数:",I)
+        if T > E:
+            print("猜小了")
+        else:
+            print("猜大了")
+    if I == B:
+        print("GAME OVER")
+        break
+
+
+
+'''
+i=0
+while i<12:
+    i=i+1
+    if i==3:
+        continue
+    print(i)
+'''
